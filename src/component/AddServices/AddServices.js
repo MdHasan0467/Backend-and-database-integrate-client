e253fd25b3d2409db1d25852d04e30ab0a3d6e9f
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-11-09 03:11:41 +0600

    Add services section added

--- src/component/AddServices/AddServices.js
+++ src/component/AddServices/AddServices.js
@@ -1,30 +1,86 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../context/AuthProvider';
-import useTitle from '../../hooks/useTitle';
+// import React, { useContext, useState } from 'react';
+// import { AuthContext } from '../../context/AuthProvider';
+// import useTitle from '../../hooks/useTitle';
+
+import { useState } from 'react';
 
 const AddServices = () => {
-    const { loading } = useContext(AuthContext);
-    useTitle('Add Services');
+	const [addservices, setAddservices] = useState({});
 
-    if (loading) {
-			return <progress className='progress progress-error w-56'></progress>;
-		}
-    return (
-			<div>
-				<h1>Add Services</h1>
-				<form>
-					<input type='text' name='img' id='' />
-					<br />
-					<input type='text' name='title' id='' />
-					<br />
-					<input type='text' name='fee' id='' />
-					<br />
-					<input type='text' name='description' id='' />
-					<br />
-					<button>Add Services</button>
-				</form>
-			</div>
-		);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(addservices);
+
+		fetch('http://localhost:5000/services', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify(addservices), //! This is user come from useState
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				// console.log(data);
+				if (data.acknowledged) {
+					alert('Service added successfully!!');
+					e.target.reset();
+				}
+			});
+	};
+	const handleBlue = (e) => {
+		const fieldName = e.target.name;
+		const value = e.target.value;
+		const newServices = { ...addservices };
+		newServices[fieldName] = value;
+		setAddservices(newServices);
+	};
+	return (
+		<div className='mt-10'>
+			<form onSubmit={handleSubmit}>
+				<div className='group'>
+					<p className='text-secondary text--2xl font-serif'>Photo URL :</p>
+			<input className= "w-[300px] h-[30px]  p-2"
+						onBlur={handleBlue}
+						type='text'
+						name='img'
+						placeholder='URL'
+						id=''
+					/>
+				</div>
+				<div className='group'>
+					<p className='text-secondary text--2xl font-serif'>Title :</p>
+			<input className= "w-[300px] h-[30px]  p-2"
+						onBlur={handleBlue}
+						type='text'
+						name='title'
+						placeholder='Title'
+						id=''
+					/>
+				</div>
+				<div className='group'>
+					<p className='text-secondary text--2xl font-serif'>Fee :</p>
+			<input className= "w-[300px] h-[30px]  p-2"
+						onBlur={handleBlue}
+						type='number'
+						name='fee'
+						placeholder='Fee'
+						id=''
+					/>
+				</div>
+				<div className='group'>
+					<p className='text-secondary text--2xl font-serif'>Description :</p>
+			<input className= "w-[300px] h-[70px] p-2"
+						onBlur={handleBlue}
+						type='text'
+						name='description'
+						placeholder='Description'
+						id=''
+					/>
+				</div>
+				<button className='btn btn-secondary my-5'>Add Services</button>
+			</form>
+		</div>
+	);
 };
 
 export default AddServices;
